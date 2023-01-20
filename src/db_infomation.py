fa9f099d0936ab05b468f335671410b924abb93d
avilesxd <nacho72001@gmail.com>
2023-01-19 23:44:49 -0300

    game and company information

---
+++ src/db_infomation.py
@@ -0,0 +1,119 @@
+Company = [
+    {
+        "id": 1,
+        "company": "Microsoft",
+        "president": "Brad Smith",
+        "founders": [
+            {
+                "name": "Bill Gates"
+
+            },
+            {
+                "name": "Paul Allen"
+            }
+        ],
+        "foundation": "April 4, 1975",
+        "Offices": [
+            {
+                "name": "Redmond"
+
+            },
+            {
+                "name": "Washington"
+            },
+            {
+                "name": "USA"
+            }
+        ]
+    },
+    {
+        "id": 2,
+        "company": "Sony Interactive Entertainment",
+        "president": "Jim Ryan",
+        "founders": [
+            {
+                "name": "Ken Kutaragi"
+
+            },
+            {
+                "name": "Norio Ohga"
+            }
+        ],
+        "foundation": "November 16, 1993",
+        "Offices": [
+            {
+                "name": "San mateo"
+
+            },
+            {
+                "name": "California"
+            },
+            {
+                "name": "USA"
+            }
+        ]
+    }
+]
+
+Games = [
+    {
+        "id": 1,
+        "name": "PES 2016",
+        "price": "49.900",
+        "console": "Xbox one"
+    },
+    {
+        "id": 2,
+        "name": "God of War Ragnarok",
+        "price": "79.990",
+        "console": "PlayStation 5"
+    },
+    {
+        "id": 3,
+        "name": "WatchDogs Legion",
+        "price": "29.990",
+        "console": "Xbox One/X"
+    },
+    {
+        "id": 4,
+        "name": "FIFA 23",
+        "price": "84.990",
+        "console": "PlayStation 5"
+    },
+    {
+        "id": 5,
+        "name": "Mafia Definitive Edition",
+        "price": "29.900",
+        "console": "Xbox One"
+    },
+    {
+        "id": 6,
+        "name": "The Last Of Us Part 1",
+        "price": "84.990",
+        "console": "PlayStation 5"
+    },
+    {
+        "id": 7,
+        "name": "FIFA 21 NXT LVL",
+        "price": "69.990",
+        "console": "Xbox One S"
+    },
+    {
+        "id": 8,
+        "name": "Crash Team Racing",
+        "price": "44.990",
+        "console": "PlayStation 4"
+    },
+    {
+        "id": 9,
+        "name": "F1 22",
+        "price": "69.990",
+        "console": "Xbox One"
+    },
+    {
+        "id": 10,
+        "name": "Spiderman Miles Morales",
+        "price": "54.990",
+        "console": "PlayStation 5"
+    },
+]
---
+++ src/db_routes.py
@@ -0,0 +1,24 @@
+Routes = [
+    {
+        "endpoint": '/games',
+        "description": 'Returns a list with all games',
+        "parameters": [
+            {
+                "name": 'Videogame',
+                "endpoint": '/games/<id>',
+                "description": 'Returns all the information about a specific game'
+            }
+        ]
+    },
+    {
+        "endpoint": '/companies',
+        "description": 'Returns a list with all companies',
+        "parameters": [
+            {
+                "name": 'Company',
+                "endpoint": '/companies/<id>',
+                "description": 'Returns all the information about a specific company'
+            }
+        ]
+    }
+]
