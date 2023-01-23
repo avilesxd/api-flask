5e81b613dcc7ef0db365ef7d0884518dd56a75c0
avilesxd <nacho72001@gmail.com>
2023-01-22 23:34:10 -0300

    separation of data used as database

---
+++ src/database/db_company.py
@@ -0,0 +1,56 @@
+Company = [
+    {
+        "id": 1,
+        "company": "Microsoft",
+        "president": "Brad Smith",
+        "founders": [
+            {
+                "name": "Bill Gates"
+
+            },
+            {
+                "name": "Paul Allen"
+            }
+        ],
+        "foundation": "April 4, 1975",
+        "Offices": [
+            {
+                "name": "Redmond"
+
+            },
+            {
+                "name": "Washington"
+            },
+            {
+                "name": "USA"
+            }
+        ]
+    },
+    {
+        "id": 2,
+        "company": "Sony Interactive Entertainment",
+        "president": "Jim Ryan",
+        "founders": [
+            {
+                "name": "Ken Kutaragi"
+
+            },
+            {
+                "name": "Norio Ohga"
+            }
+        ],
+        "foundation": "November 16, 1993",
+        "Offices": [
+            {
+                "name": "San mateo"
+
+            },
+            {
+                "name": "California"
+            },
+            {
+                "name": "USA"
+            }
+        ]
+    }
+]
