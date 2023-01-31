9d29613340a22969a304ddfbfaab86b060b5c51c
avilesxd <nacho72001@gmail.com>
2023-01-31 11:13:35 -0300

    new order

--- src/database/db_games.py
+++ src/database/db_games.py
@@ -1,62 +1,62 @@
 Games = [
     {
         "id": 1,
-        "name": "PES 2016",
-        "price": "49.900",
-        "console": "Xbox one"
-    },
-    {
-        "id": 2,
         "name": "God of War Ragnarok",
         "price": "79.990",
         "console": "PlayStation 5"
     },
     {
-        "id": 3,
-        "name": "WatchDogs Legion",
-        "price": "29.990",
-        "console": "Xbox One/X"
-    },
-    {
-        "id": 4,
+        "id": 2,
         "name": "FIFA 23",
         "price": "84.990",
         "console": "PlayStation 5"
     },
     {
-        "id": 5,
-        "name": "Mafia Definitive Edition",
-        "price": "29.900",
-        "console": "Xbox One"
-    },
-    {
-        "id": 6,
+        "id": 3,
         "name": "The Last Of Us Part 1",
         "price": "84.990",
         "console": "PlayStation 5"
     },
     {
-        "id": 7,
-        "name": "FIFA 21 NXT LVL",
-        "price": "69.990",
-        "console": "Xbox One S"
-    },
-    {
-        "id": 8,
+        "id": 4,
         "name": "Crash Team Racing",
         "price": "44.990",
         "console": "PlayStation 4"
     },
     {
-        "id": 9,
-        "name": "F1 22",
-        "price": "69.990",
-        "console": "Xbox One"
-    },
-    {
-        "id": 10,
+        "id": 5,
         "name": "Spiderman Miles Morales",
         "price": "54.990",
         "console": "PlayStation 5"
+    },
+    {
+        "id": 6,
+        "name": "PES 2016",
+        "price": "49.900",
+        "console": "Xbox one"
+    },
+    {
+        "id": 7,
+        "name": "WatchDogs Legion",
+        "price": "29.990",
+        "console": "Xbox One/X"
+    },
+    {
+        "id": 8,
+        "name": "Mafia Definitive Edition",
+        "price": "29.900",
+        "console": "Xbox One"
+    },
+    {
+        "id": 9,
+        "name": "FIFA 21 NXT LVL",
+        "price": "69.990",
+        "console": "Xbox One S"
+    },
+    {
+        "id": 10,
+        "name": "F1 22",
+        "price": "69.990",
+        "console": "Xbox One"
     },
 ]
