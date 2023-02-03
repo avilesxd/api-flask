541be6e723641a6006970f6a6d231bf712795bc1
avilesxd <nacho72001@gmail.com>
2023-02-03 10:35:36 -0300

    new test

--- src/tests/test_routes_api.py
+++ src/tests/test_routes_api.py
@@ -2,35 +2,38 @@ import random
 import unittest
 import requests
 
+from flask import Flask
+app = Flask(__name__)
+
 
 class TestAPI(unittest.TestCase):
 
     def test_route_to_get_the_games(self):
         req = requests.get("http://127.0.0.1:5000/games")
-        print(f"Games route:", req.status_code)
         self.assertEqual(req.status_code, 200)
 
     def test_route_to_get_the_specific_game(self):
         id = random.randint(1, 10)
         req = requests.get(f"http://127.0.0.1:5000/games/{id}")
-        print(f"game path with unique id:", req.status_code)
         self.assertEqual(req.status_code, 200)
 
     def test_route_to_get_the_companies(self):
         req = requests.get("http://127.0.0.1:5000/companies")
-        print(f"Companies route:", req.status_code)
         self.assertEqual(req.status_code, 200)
 
     def test_route_to_get_the_company(self):
         id = random.randint(1, 2)
         req = requests.get(f"http://127.0.0.1:5000/companies/{id}")
-        print(f"company path with unique id:", req.status_code)
         self.assertEqual(req.status_code, 200)
 
     def test_route_to_get_the_photos(self):
         req = requests.get("http://127.0.0.1:5000/photos")
-        print(f"Photos route:", req.status_code)
         self.assertEqual(req.status_code, 200)
+
+    def test_route_not_exists(self):
+        with app.test_client() as c:
+            response = c.get('http://127.0.0.1:5000/copanies/42')
+            self.assertEqual(response.status_code, 404)
 
 
 if __name__ == "__main__":
